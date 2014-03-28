0ce676150ab35892de490dc0cf177162f16e4d58
Jeffrey Nelson <jeff.nelson@clever.com>
2014-03-27 19:42:41 -0700

    Added test containing odd padding.

--- test/xmlenc.encryptedkey.js
+++ test/xmlenc.encryptedkey.js
@@ -98,4 +98,12 @@ describe('encrypt', function() {
     });
   });
 
+  it('should decrypt xml with odd padding (aes256-cbc)', function (done) {
+    var encryptedContent = fs.readFileSync(__dirname + '/test-cbc256-padding.xml').toString()
+    xmlenc.decrypt(encryptedContent, { key: fs.readFileSync(__dirname + '/test-auth0.key')}, function(err, decrypted) {
+      assert.equal(decrypted, 'content');
+      done();
+    });
+  });
+
 });
