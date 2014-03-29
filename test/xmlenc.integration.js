511d94399551d1c6216907c15937f8947ef58f62
Matias Woloski <matiasw@gmail.com>
2014-03-29 09:46:36 -0300

    remove autopadding prop on test

--- test/xmlenc.integration.js
+++ test/xmlenc.integration.js
@@ -12,7 +12,7 @@ describe('integration', function() {
   it('should decrypt assertion with aes128', function (done) {
     var result = fs.readFileSync(__dirname + '/assertion-sha1-128.xml').toString();
 
-    xmlenc.decrypt(result, { autopadding: false, key: fs.readFileSync(__dirname + '/test-cbc128.key')}, function (err, decrypted) {
+    xmlenc.decrypt(result, { key: fs.readFileSync(__dirname + '/test-cbc128.key')}, function (err, decrypted) {
       // decrypted content should finish with <saml2:Assertion>
       assert.equal(/<\/saml2:Assertion>$/.test(decrypted), true);
       done();
