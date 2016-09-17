e63d8cee07beec3df91e034f5bcd48e43213e4a1
Xavier Dumesnil <xavier@frontapp.com>
2016-09-16 18:46:47 -0700

    Support padding for tripledes-cbc algorithm

--- test/xmlenc.encryptedkey.js
+++ test/xmlenc.encryptedkey.js
@@ -12,7 +12,7 @@ describe('encrypt', function() {
     // cert created with:
     // openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/CN=auth0.auth0.com/O=Auth0 LLC/C=US/ST=Washington/L=Redmond' -keyout auth0.key -out auth0.pem
     // pub key extracted from (only the RSA public key between BEGIN PUBLIC KEY and END PUBLIC KEY)
-    // openssl x509 -in "test-auth0.pem" -pubkey 
+    // openssl x509 -in "test-auth0.pem" -pubkey
 
     var options = {
       rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
@@ -22,7 +22,7 @@ describe('encrypt', function() {
       keyEncryptionAlgorighm: 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p'
     };
 
-    xmlenc.encrypt('content to encrypt', options, function(err, result) {  
+    xmlenc.encrypt('content to encrypt', options, function(err, result) {
       xmlenc.decrypt(result, { key: fs.readFileSync(__dirname + '/test-auth0.key')}, function(err, decrypted) {
         assert.equal(decrypted, 'content to encrypt');
         done();
@@ -34,7 +34,7 @@ describe('encrypt', function() {
     // cert created with:
     // openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/CN=auth0.auth0.com/O=Auth0 LLC/C=US/ST=Washington/L=Redmond' -keyout auth0.key -out auth0.pem
     // pub key extracted from (only the RSA public key between BEGIN PUBLIC KEY and END PUBLIC KEY)
-    // openssl x509 -in "test-auth0.pem" -pubkey 
+    // openssl x509 -in "test-auth0.pem" -pubkey
 
     var options = {
       rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
@@ -44,7 +44,7 @@ describe('encrypt', function() {
       keyEncryptionAlgorighm: 'http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p'
     };
 
-    xmlenc.encrypt('Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge', options, function(err, result) {  
+    xmlenc.encrypt('Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge', options, function(err, result) {
       xmlenc.decrypt(result, { key: fs.readFileSync(__dirname + '/test-auth0.key')}, function(err, decrypted) {
         assert.equal(decrypted, 'Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge');
         done();
@@ -56,7 +56,7 @@ describe('encrypt', function() {
     // cert created with:
     // openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/CN=auth0.auth0.com/O=Auth0 LLC/C=US/ST=Washington/L=Redmond' -keyout auth0.key -out auth0.pem
     // pub key extracted from (only the RSA public key between BEGIN PUBLIC KEY and END PUBLIC KEY)
-    // openssl x509 -in "test-auth0.pem" -pubkey 
+    // openssl x509 -in "test-auth0.pem" -pubkey
 
     var options = {
       rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
@@ -95,7 +95,7 @@ describe('encrypt', function() {
     // cert created with:
     // openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/CN=auth0.auth0.com/O=Auth0 LLC/C=US/ST=Washington/L=Redmond' -keyout auth0.key -out auth0.pem
     // pub key extracted from (only the RSA public key between BEGIN PUBLIC KEY and END PUBLIC KEY)
-    // openssl x509 -in "test-auth0.pem" -pubkey 
+    // openssl x509 -in "test-auth0.pem" -pubkey
 
     var options = {
       rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
@@ -105,13 +105,35 @@ describe('encrypt', function() {
       keyEncryptionAlgorighm: 'http://www.w3.org/2001/04/xmlenc#rsa-1_5'
     };
 
-    xmlenc.encrypt('content to encrypt', options, function(err, result) {  
+    xmlenc.encrypt('content to encrypt', options, function(err, result) {
       xmlenc.decrypt(result, { key: fs.readFileSync(__dirname + '/test-auth0.key')}, function(err, decrypted) {
         assert.equal(decrypted, 'content to encrypt');
         done();
       });
     });
   });
+
+  it('should encrypt and decrypt xml (encryption: http://www.w3.org/2001/04/xmlenc#tripledes-cbc, keyEncryption: http://www.w3.org/2001/04/xmlenc#rsa-1_5)', function (done) {
+      // cert created with:
+      // openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/CN=auth0.auth0.com/O=Auth0 LLC/C=US/ST=Washington/L=Redmond' -keyout auth0.key -out auth0.pem
+      // pub key extracted from (only the RSA public key between BEGIN PUBLIC KEY and END PUBLIC KEY)
+      // openssl x509 -in "test-auth0.pem" -pubkey
+
+      var options = {
+        rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
+        pem: fs.readFileSync(__dirname + '/test-auth0.pem'),
+        key: fs.readFileSync(__dirname + '/test-auth0.key'),
+        encryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#tripledes-cbc',
+        keyEncryptionAlgorighm: 'http://www.w3.org/2001/04/xmlenc#rsa-1_5'
+      };
+
+      xmlenc.encrypt('Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge', options, function (err, result) {
+        xmlenc.decrypt(result, { key: fs.readFileSync(__dirname + '/test-auth0.key')}, function (err, decrypted) {
+          assert.equal(decrypted, 'Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge Gnügge');
+          done();
+        });
+      });
+    });
 
   it('should encrypt and decrypt keyinfo', function (done) {
     var options = {
@@ -122,7 +144,7 @@ describe('encrypt', function() {
 
     crypto.randomBytes(32, function(err, randomBytes) {
       if (err) return done(err);
-      xmlenc.encryptKeyInfo(randomBytes, options, function(err, result) { 
+      xmlenc.encryptKeyInfo(randomBytes, options, function(err, result) {
         if (err) return done(err);
         var decryptedRandomBytes = xmlenc.decryptKeyInfo(result, { key: fs.readFileSync(__dirname + '/test-auth0.key')});
 
